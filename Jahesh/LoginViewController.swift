3cfeee0ddcb6b582cd0ead60726c79af0fad6ece
Arash Farahani <arashfarahani@Arashs-Mac.local>
2017-04-09 23:38:38 +0430

    First Lesson

--- Jahesh/LoginViewController.swift
+++ Jahesh/LoginViewController.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2017 Arash Farahani. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    var usernameTextField: UITextField!
+    var passwordTextField: UITextField!
+    var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        usernameTextField = UITextField(frame: CGRect(x: 100, y: 100, width: 70, height: 50))
+        usernameTextField.text = "test"
+        view.addSubview(usernameTextField)
+        
+        loginButton = UIButton(frame: CGRect(x: 100, y: 170, width: 70, height: 50))
+        loginButton.setTitleColor(.blue, for: .normal)
+        loginButton.setTitle("Login", for: UIControlState.normal)
+        loginButton.addTarget(self, action: #selector(LoginViewController.buttonClicked), for: .touchUpInside)
+        
+        view.addSubview(loginButton)
+        
+        
+    }
+    
+    func buttonClicked() {
+        
+    }
+
+
+}
--- Jahesh/ViewController.swift
+++ Jahesh/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func loginButtonClick(_ sender: Any) {
+        if usernameTextField.text == "admin" && passwordTextField.text == "admin" {
+            // show alert welcome to the club
+            
+            let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
+
+            alert.addAction(UIAlertAction(title: "Click", style: .default, handler: { (action) in
+                
+                
+                self.usernameTextField.text = "hello"
+                
+            }))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+
+    }
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        passwordTextField.isSecureTextEntry = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
